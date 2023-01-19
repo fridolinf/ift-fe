dee397f2c9c56db5a6367004f973800182996be7
fridolinf <frelly130@gmail.com>
2023-01-20 01:31:11 +0800

    remove unused change title html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
 const content = ["./src/**/*.{js,jsx}"];
-const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: content,
@@ -8,10 +7,6 @@ module.exports = {
       colors: {
         myPrimary: "#b8eaf6",
       },
-      // colors: {
-      //   rose: colors.rose,
-      //   purple: colors.purple,
-      // },
     },
   },
   plugins: [],
